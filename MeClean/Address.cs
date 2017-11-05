5d52182c42eb16461483af5e3d649af7a7e2b4cf
Adam Rogers <Adam Rogers>
2017-11-05 22:38:37 +0000

    Add project files.

---
+++ MeClean/Address.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MeClean
+{
+    class Address
+
+    {
+        protected string _address1;
+        protected string address2;
+        protected string address3;
+        protected string city;
+        protected string county;
+        protected string _postcode;
+
+        public string Addressline1
+        {
+            get
+            {
+                return _address1;
+            }
+            set
+            {
+                _address1 = value;
+            }
+
+        }
+
+        public string Postcode
+        {
+            get
+            {
+                return _postcode;
+            }
+            set
+            {
+                string postcode = "([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\\s?[0-9][A-Za-z]{2})";
+
+                Regex regex = new Regex(postcode, RegexOptions.IgnoreCase);
+                Match match = regex.Match(value);
+                if (match.Success)
+                {
+                    _postcode = match.Value;
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ MeClean/UploadForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace MeClean
+{
+    public partial class UploadForm : Form
+    {
+        public UploadForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Address address = new Address();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                List<string> rows = File.ReadAllLines(filename).ToList();
+
+                int max = Int32.MinValue;
+
+                int columnindex = 0;
+                int rowindex = 0;
+                
+                foreach (string row in rows)
+                {
+
+
+                    foreach (string column in row.Split(','))
+                    {
+                        var CsvList = new List<Csv>();
+                        CsvList.Add(new Csv {
+                            rownumber = rowindex,
+                            columnnumber = columnindex,
+                            columncontent = column,
+                            postcodecontent = address.Postcode
+
+
+                        });
+                        Csv firstCsv = CsvList[0];
+                        MessageBox.Show(firstCsv.rownumber.ToString() + " " + firstCsv.columncontent.ToString());
+                        columnindex++;
+                    }
+                    columnindex = 0;
+                    rowindex++;
+                }
+
+
+               
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+       
+    }
+
+
